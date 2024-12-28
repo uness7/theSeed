a36f779cd7a5ac83f057dc3649c4410d109f9a86
Youness Zioual <crness2@gmail.com>
2024-12-28 15:23:35 +0100

    add: created a new app user for auth system

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'dashboard.apps.DashboardConfig',
+    'user.apps.UserConfig',
 ]
 
 MIDDLEWARE = [
