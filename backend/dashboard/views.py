b678dbbe25dc1d65daf4edfd1922a6e4c909ffa9
Youness Zioual <crness2@gmail.com>
2024-12-28 15:10:55 +0100

    mod: renamed core/ to config/ inside backend folder

--- backend/dashboard/views.py
+++ backend/dashboard/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def my_view(request):
-    return HttpResponse("Hello, world!")
+	return HttpResponse("Hello, world!")
 
+def my_another_view(request):
+	return HttpResponse("Another one!");
